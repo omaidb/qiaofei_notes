7ad963bd80601e89aa5acef29c14ce624308a84a
qiaofei <omaidb@gmail.com>
2023-06-20 02:49:27 -0400

    快速push

--- shell_code/infosec/install_denyhosts.sh
+++ shell_code/infosec/install_denyhosts.sh
@@ -6,18 +6,31 @@ pkg_version=2.10
 ## 检查依赖环境
 check_pkg_env(){
     # 检查python3是否安装
+    command -v python|| yum install -y python
     command -v python3|| yum install -y python3
 }
 
 ## 下载解压
 down_unzip_pkg(){
     # https://blog.51cto.com/feko/2735467
+    cd /usr/local/src/ || exit
     wget -P /usr/local/src/ https://github.com/denyhosts/denyhosts/archive/v$pkg_version.tar.gz
     tar xf /usr/local/src/v$pkg_version.tar.gz
 }
 
 ## 安装
 install_pkg(){
-    cd /usr/local/src/denyhosts-$pkg_version
+    cd /usr/local/src/denyhosts-$pkg_version || exit
     python setup.py install
+    cp /usr/local/src/denyhosts-$pkg_version/denyhosts.service /etc/systemd/system/
+    systemctl daemon-reload
 }
+
+## 执行主方法
+main() {
+    check_pkg_env
+    down_unzip_pkg
+    install_pkg
+}
+
+main
