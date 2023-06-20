40ef1241b98f0177c5df8bd3e6c24b6c25aa793e
qiaofei <omaidb@gmail.com>
2023-06-20 03:42:47 -0400

    快速push

--- shell_code/infosec/install_denyhosts.sh
+++ shell_code/infosec/install_denyhosts.sh
@@ -24,7 +24,9 @@ install_pkg(){
     python setup.py install || python2 setup.py install
     # 创建PID文件
     touch /var/run/denyhosts.pid
-    # 下载配置文件
+    # 备份配置
+    mv /etc/denyhosts.conf /etc/denyhosts.conf.bak
+    # 下载正式配置文件
     wget -P /etc/ -c https://raw.githubusercontent.com/omaidb/qiaofei_notes/main/config_bak/denyhosts.conf
     # 复制服务文件
     cp /usr/local/src/denyhosts-$pkg_version/denyhosts.service /etc/systemd/system/
