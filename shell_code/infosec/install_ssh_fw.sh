48a82ddb7712013dca7906353b2be6463ef1e728
qiaofei <omaidb@gmail.com>
2023-06-19 23:07:06 -0400

    快速push

--- shell_code/infosec/install_ssh_fw.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-# 下载ssh自动拉黑IP脚本
-mkdir -p /opt/block_ssh_ip
-wget -P /opt/block_ssh_ip -c https://raw.githubusercontent.com/omaidb/qiaofei_notes/main/shell_code/infosec/block_ssh_error_ip.sh
