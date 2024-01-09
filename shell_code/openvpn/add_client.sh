a7fd8bf378182122bd294cea3092f67aa52733d8
自然自在 <omaidb@gmail.com>
2024-01-09 18:03:04 +0800

    add_openvpn_client

---
+++ shell_code/openvpn/add_client.sh
@@ -0,0 +1,135 @@
+#! /bin/bash
+
+## 获取公网IP地址
+IP_ADD=$(curl -s ipv4.icanhazip.com | xargs echo -n)
+# 总共接受的参数数量
+para_num=$#
+# 第一个参数 是add 或del
+operation=$1
+# 客户端名称
+vpn_user=$2
+# 客户端配置目录
+user_path=/etc/openvpn/client/$vpn_user
+# 客户端配置文件
+client_conf_file=${user_path}/${vpn_user}.ovpn
+# 客户端证书文件
+client_crt_file=/etc/openvpn/easy-rsa/pki/issued/${vpn_user}.crt
+# 客户端私钥文件
+client_key_file=/etc/openvpn/easy-rsa/pki/private/${vpn_user}.key
+# CA证书文件
+ca_crt_file=/etc/openvpn/easy-rsa/pki/ca.crt
+# DH证书文件
+# dh_crt_file=/etc/openvpn/easy-rsa/pki/dh.pem
+# easyrsa程序目录
+easyrsa_dir=/etc/openvpn/easy-rsa
+# easyrsa程序路径
+easyrsa_exe=$easyrsa_dir/easyrsa
+
+# 帮助信息
+function help() {
+    echo './openvpn_user.sh add username(vpn username)'
+    echo './openvpn_user.sh del username(vpn username)'
+}
+
+# 添加用户方法
+function add_user() {
+    if [ -f "$client_crt_file" ]; then
+        echo "$vpn_user 已经存在!"
+        exit 1
+    else
+        cd $easyrsa_dir || exit
+        # 非交互模式生成客户端证书
+        $easyrsa_exe --batch build-client-full "$vpn_user" nopass
+    fi
+}
+
+# 删除用户方法
+function del_user() {
+    cd $easyrsa_dir || exit
+    # 非交互式撤销用户证书
+    $easyrsa_exe --batch revoke "$vpn_user"
+    # 删除用户配置目录
+    rm -rf "$user_path"
+    echo "================================success================================"
+    echo "del vpn user: $vpn_user success!"
+    echo "================================success================================"
+}
+
+# 生成client.conf
+function generate_client_conf() {
+    # client配置文件路径
+    mkdir -p "$user_path"
+    cd "$user_path" || exit
+
+    if [ -f "$client_conf_file" ]; then
+        echo "$client_conf_file 已经存在!"
+        exit 1
+
+    fi
+
+    # 生成客户端配置文件
+    {
+        echo "client"
+        echo "dev tun"
+        echo "proto tcp"
+        echo "remote $IP_ADD 1194"
+        echo "resolv-retry infinite"
+        echo "nobind"
+        echo "persist-key"
+        echo "persist-tun"
+        echo "compress lz4-v2"
+        echo "keysize 128"
+        echo "verb 5"
+        echo "remote-cert-tls server"
+        echo "tls-version-min 1.2"
+        echo "tls-version-max 1.2"
+        echo "tls-cipher TLS-DHE-RSA-WITH-AES-256-GCM-SHA384:TLS-DHE-RSA-WITH-AES-256-CBC-SHA256:TLS-DHE-RSA-WITH-AES-128-GCM-SHA256:TLS-DHE-RSA-WITH-AES-128-CBC-SHA256"
+        echo "auth-nocache"
+        echo "key-direction 1"
+        echo "dhcp-option DNS 1.1.1.1"
+        echo "dhcp-option DNS 8.8.8.8"
+        echo "redirect-gateway def1 bypass-dhcp"
+        echo "route-delay 2"
+        echo "keepalive 10 60"
+
+        echo "<ca>"
+        cat ${ca_crt_file}
+        echo "</ca>"
+
+        echo "<cert>"
+        tail -n 31 "${client_crt_file}"
+        echo "</cert>"
+
+        echo "<key>"
+        cat "${client_key_file}"
+        echo "</key>"
+    } >>"${client_conf_file}"
+}
+
+# 主方法
+function main() {
+    # 当参数不等于2
+    if [ $para_num -ne 2 ]; then
+        echo "参数非法!"
+        help
+        exit 1
+    else
+        if [ "$operation" = "add" ]; then
+            add_user
+            # 生成客户端配置文件
+            generate_client_conf
+            # 打印添加vpn客户端成功
+            echo "===============================success=================================="
+            echo "add vpn user: $vpn_user success!"
+            echo "===============================success=================================="
+        elif [ "$operation" = "del" ]; then
+            del_user
+        else
+            echo '第一个参数仅支持 add 或 del'
+            help
+            exit 1
+        fi
+    fi
+}
+
+main
